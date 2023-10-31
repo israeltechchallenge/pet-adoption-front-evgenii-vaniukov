c596f5c1b417ad7f906abe6eb772e44cf1e9712d
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-10-31 17:57:46 +0200

    test

--- src/features/homepage/pages/home_logged_in.jsx
+++ src/features/homepage/pages/home_logged_in.jsx
@@ -1,7 +1,7 @@
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 import { CTA } from "../components/cta_section";
-
+// test
 export function HomeLoggedIn() {
   return (
     <div className="bg-white">
